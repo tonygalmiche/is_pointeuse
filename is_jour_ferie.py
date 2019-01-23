65867ddf36743ff8709e52c84669ac34d82859c0
Tony Galmiche <tony.galmiche@infosaone.com>
2019-01-23 15:30:02 +0100

    add jours férié

--- is_jour_ferie.py
+++ is_jour_ferie.py
@@ -5,13 +5,13 @@ from openerp.tools.translate import _
 
 class is_jour_ferie(osv.osv):
     _name='is.jour.ferie'
-    _order='date'    #Ordre de tri par defaut des listes
+    _order='date'
 
-    #ATTENTION : Pour que la relation many2one affiche le code du moule, le champ doit être nommé 'name' sinon il faut surcharger la méthode name_get
     _columns={
-        'name':fields.char("Intitulé",size=100,help='Intitulé du jour férié (ex : Pâques)', required=True, select=True),
-        'date':fields.date("Date",required=True),
+        'name'     : fields.char("Intitulé",size=100,help='Intitulé du jour férié (ex : Pâques)', required=True, select=True),
+        'date'     : fields.date("Date",required=True),
         'jour_fixe': fields.boolean('jour férié fixe',  help="Cocher pour préciser que ce jour férié est valable tous les ans"),
+        'info_id'  : fields.many2one('is.heure.effective.info', 'Information'),
     }
 
 is_jour_ferie()
